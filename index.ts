7e6741d76c729797950b2bf18557337390007b4a
jeg <jeg@knapsack>
2024-07-04 09:22:21 -0400

    eslint and box-sizing changes

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import Lollygag from '@lollygag/core';
-import livedev from '@lollygag/livedev';
-import markdown from '@lollygag/markdown';
-import templates from '@lollygag/templates';
-import sass from '@lollygag/sass';
+import Lollygag from '@lollygag/lollygag';
+import livedev from '@lollygag/lollygag/dist/workers/livedev';
+import markdown from '@lollygag/lollygag/dist/workers/markdown';
+import scss from '@lollygag/lollygag/dist/workers/scss';
+import templates from '@lollygag/lollygag/dist/workers/templates';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const lollygag = new Lollygag();
@@ -11,19 +11,21 @@ lollygag
     .config({
         prettyUrls: true,
     })
-    .meta({
+    .contentDir('files')
+    .outputDir('public')
+    .sitemeta({
         siteName: 'Simpleton CSS',
         siteDescription: 'A glorified CSS reset.',
     })
     .do(markdown())
     .do(templates())
-    .do(sass());
+    .do(scss());
 
-if(!isProduction) {
+if (!isProduction) {
     lollygag.do(
         livedev({
             serverPort: 3003,
-            livereloadHost: '192.168.2.111',
+            livereloadHost: 'localhost',
             livereloadPort: 35555,
             patterns: {
                 'files/**/*': true,
